408541d4e6a6f5fc6552a948462ee11ab7debe15
Shamsiddinkhuja Abdurakhimov <64980797+chebarash@users.noreply.github.com>
2024-05-14 12:03:45 +0500

    Event

--- src/app/event.tsx
+++ src/app/event.tsx
@@ -4,25 +4,27 @@ export default function Event({
   category,
   title,
   date,
+  img,
   id,
 }: {
   category: string;
   title: string;
   date: string;
+  img: string;
   id: string;
 }) {
   return (
     <section className={styles.event}>
       <span>
         <p>{category}</p>
-        <img src="Sdsds" alt="dsds" />
+        <img src={img} alt={title} />
       </span>
       <div>
-        <div>
-          <p>{title}</p>
-          <p>{date}</p>
+        <div className={styles.box}>
+          <p className={styles.title}>{title}</p>
+          <p className={styles.date}>{date}</p>
         </div>
-        <div>
+        <div className={styles.arrow}>
           <svg width="19" height="19" viewBox="0 0 19 19" fill="none">
             <path
               d="M3 16L17 2M17 2V16M17 2H3"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,15 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Event title="Test" category="Test" date="2:00 am" id="424" />
+      {Array(10).fill(
+        <Event
+          title="Gifted Hands for ACS"
+          category="Movie Night"
+          date="14:00"
+          id="424"
+          img="https://movieguide.b-cdn.net/wp-content/uploads/2014/05/Gifted-Hands-Review-Slider.jpg"
+        />
+      )}
     </main>
   );
 }
