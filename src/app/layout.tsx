4754a75b463cc15d9b9c54ba0767b102ec994589
Shamsiddinkhuja Abdurakhimov <64980797+chebarash@users.noreply.github.com>
2024-06-20 16:33:32 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script src="https://telegram.org/js/telegram-web-app.js"></script>
+      </head>
       <body className={inter.className}>
         <Header />
         {children}
