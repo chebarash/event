ec1dbb9145ce6c8af46b04eaac1b9cd6c16e8e8b
Shamsiddinkhuja Abdurakhimov <64980797+chebarash@users.noreply.github.com>
2024-06-27 10:21:21 +0500

    Update route.tsx
    Test
    
    bug fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import Script from "next/script";
 import "./globals.css";
 import Header from "./components/header";
 import { Suspense } from "react";
+import { SessProvider } from "./hooks/session";
 
 export const metadata: Metadata = {
   title: "Event",
@@ -23,10 +24,12 @@ export default function RootLayout({
         />
       </head>
       <body>
-        <Suspense>
-          <Header />
-        </Suspense>
-        {children}
+        <SessProvider>
+          <Suspense>
+            <Header />
+          </Suspense>
+          {children}
+        </SessProvider>
       </body>
     </html>
   );
