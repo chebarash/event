e77b5610b1d5d8ac35070dbd353c000a5304a317
CheBarash <chebarashek@gmail.com>
2025-04-06 11:45:46 +0500

    Election

--- src/app/clubs/page.tsx
+++ src/app/clubs/page.tsx
@@ -4,8 +4,16 @@ import Clubs from "@/ui/clubs/clubs";
 
 export const dynamic = "force-dynamic";
 
-export default async function ClubsPage() {
+export default async function ClubsPage({
+  searchParams,
+}: {
+  searchParams?: { parties?: string };
+}) {
   const { data } = await axiosInstance.get<Array<ClubType>>(`/clubs`);
 
-  return <Clubs clubs={data} />;
+  const res = searchParams?.parties
+    ? data.filter(({ _id }) => [`6797b731a0297fc8703b182c`].includes(_id))
+    : data;
+
+  return <Clubs clubs={res} />;
 }
